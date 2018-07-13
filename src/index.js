b00bc144454874528bbe7d98a852969ed8eac125
Max Medina <maxmedina05@gmail.com>
2018-07-13 13:56:09 -0400

    setup redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
