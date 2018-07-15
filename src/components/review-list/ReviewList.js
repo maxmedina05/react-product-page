e09fff48ab00dd42a43a3b86a2f3139c561b9894
Max Medina <maxmedina05@gmail.com>
2018-07-15 10:11:11 -0400

    Refactor review list

--- src/components/review-list/ReviewList.js
+++ src/components/review-list/ReviewList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ReviewListRow from './ReviewListRow';
 import ReviewListRowHeader from './ReviewListRowHeader';
